b341975c2164cf164e2bb7e2886eba80f8c8b1e1
Veronica Lim <limveronica94@gmail..com>
2023-05-11 10:52:35 -0400

    Refactor Luno Premium Part 1

--- stretchGoal.js
+++ stretchGoal.js
@@ -1,47 +1,29 @@
-// npm install -s node-binance-api
 // npm install dotenv
 // npm install prompt-sync // NEW
-import Binance from 'node-binance-api'; // making API calls to Binance
-const binance = new Binance(); // new constructor
-import dotenv from 'dotenv';
-dotenv.config();
-import promptSync from 'prompt-sync';
-const prompt = new promptSync({sigint: true});
+
+import { getLunoBTCPriceInMYR } from './lib/luno.js';
+import { getExchangeRate } from './lib/exchange-rate.js';
+import { getBinanceBTCPriceInUSD } from './lib/binance.js';
 
 async function lunoProcess() {
-  let currency = prompt("Enter a valid cryptocurrency: ")// NEW
- 
-  // BTCMYR price from luno:
-  const url = "https://api.luno.com/api/1/ticker?pair=" + currency + "MYR" // NEW
-  const luno = await fetch(url); // NEW
-  const lunoResult = await luno.json();
-  const btcMyr = Math.trunc(lunoResult.last_trade);
+  // Get Luno BTC price in MYR:
+  const btcMyr = await getLunoBTCPriceInMYR();
   console.log(`BTCMYR price on Luno:`.padEnd(30) + `MYR ${btcMyr}`);
   
   // Exchange rate: 
-  const myHeaders = new Headers();
-  myHeaders.append("apikey", process.env.API_VALUE);
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-  };
-    const conv = await fetch ("https://api.apilayer.com/fixer/latest?symbols=MYR&base=USD", requestOptions);
-    const convResult = await conv.json();
-    const rate = convResult.rates.MYR;
-    console.log(`USDMYR:`.padEnd(30) + `${rate}`);
+  const rate = await getExchangeRate();
+  console.log(`USDMYR:`.padEnd(30) + `${rate}`);
 
   // BTCUSD price on Luno:
   const btcUsd = btcMyr/rate;
   console.log(`BTCUSD price on Luno:`.padEnd(30) + `USD ${btcUsd}`);
   
-  // BTCUSD price on Binance:
-  const ticker = await binance.prices();
-  const btcbUsd = ticker.BTCUSDT;
+  // Get Binance BTC price in USD:
+  const btcbUsd = await getBinanceBTCPriceInUSD();
   console.log(`BTCUSD price on Binance:`.padEnd(30) + `USD ${btcbUsd}`);
   
   // Price difference:
-  const priceDiff = btcUsd - ticker.BTCUSDT;
+  const priceDiff = btcUsd - btcbUsd;
   console.log(`Price difference:`.padEnd(30) + `USD ${priceDiff}`);
 
   // Luno premium:
